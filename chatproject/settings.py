70bf82f99835c49d69f25b538af9d89c739c9076
Chaitanya Malani <58743317+cmn22@users.noreply.github.com>
2022-04-30 02:13:24 +0530

    Update settings.py

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -133,7 +133,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
+STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
